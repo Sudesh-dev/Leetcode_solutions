ae3f7d6541513f96fc5422023aead71f0f661165
Sudesh <sudeshkr861@gmail.com>
2026-01-22 12:54:22 +0530

    q175

---
+++ Databases/p_175.sql
@@ -0,0 +1,5 @@
+/* Write a solution to report the first name, last name, city, and state of each person in the Person table.
+ If the address of a personId is not present in the Address table, report null instead.*/
+select firstName, lastName,city, state 
+from 
+Person left join Address on Person.personId = Address.personId
